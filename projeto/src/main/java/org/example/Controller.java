91f4225a9802721ebf2420782540d14286efd83b
lhmontech <lhmonteiro.ti@gmail.com>
2026-04-02 23:01:52 -0300

    chore: Populei com dados de exemplo a tabela de planejamento

--- projeto/src/main/java/org/example/Controller.java
+++ projeto/src/main/java/org/example/Controller.java
@@ -1,12 +1,38 @@
 package org.example;
 
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class Controller {
+public class Controller implements Initializable {
+
+    @FXML
+    private void clicarBtnVoltar() {
+        try {
+            App.setRoot("TelaDisciplinas");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void clicarDisciplina() {
+        try {
+            App.setRoot("TelaPlanejamento");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     private ComboBox<String> cbEscolherDisciplina;
@@ -14,34 +40,52 @@ public class Controller {
     @FXML
     private ComboBox<String> cbEscolherTurma;
 
+
     @FXML
-    private TableView<Object> tabelaCronograma;
+    void escolherDicilplina(ActionEvent event) {
 
+    }
 
-@FXML
-void escolherDicilplina(ActionEvent event) {
-
-}
-
-@FXML
-void escolherTurma(ActionEvent event) {
-
-}
     @FXML
-        private void clicarBtnVoltar() {
-            try {
-                App.setRoot("TelaDisciplinas");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+    void escolherTurma(ActionEvent event) {
 
-        @FXML
-        private void clicarDisciplina() {
-            try {
-                App.setRoot("TelaPlanejamento");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    }
+
+    @FXML
+    private TableView<Planejamento> tabelaCronograma;
+
+    @FXML
+    private TableColumn<Planejamento, String> colData;
+
+    @FXML
+    private TableColumn<Planejamento, String> colDia;
+
+    @FXML
+    private TableColumn<Planejamento, String> colHorario;
+
+    @FXML
+    private TableColumn<Planejamento, String> colEvento;
+
+    @FXML
+    private TableColumn<Planejamento, String> colConteudo;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        if (colData != null){
+            colData.setCellValueFactory(new PropertyValueFactory<>("data"));
+            colDia.setCellValueFactory(new PropertyValueFactory<>("dia"));
+            colHorario.setCellValueFactory(new PropertyValueFactory<>("horario"));
+            colEvento.setCellValueFactory(new PropertyValueFactory<>("evento"));
+            colConteudo.setCellValueFactory(new PropertyValueFactory<>("conteudo"));
+            tabelaCronograma.setItems(getDadosExemplo());
         }
+    }
+
+    private ObservableList<Planejamento> getDadosExemplo() {
+        return FXCollections.observableArrayList(
+                new Planejamento("02/03/2026","Segunda","18:45 - 19:35","Letivo","Selecionar"),
+                new Planejamento("04/03/2026","Quarta","18:45 - 19:35","Letivo","Selecionar"),
+                new Planejamento("04/03/2026","Quarta","19:35 - 20:25","Letivo","Selecionar")
+                );
+    }
 }
